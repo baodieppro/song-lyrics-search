0e3ea3862df13116f5298d5ebe3e231c0478e6cc
laugeeme <registro.laura@gmail.com>
2020-06-19 13:08:38 +0200

    add selectors in interfaz module

---
+++ js/app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import * as UI from './interfaz.js';
+
+console.log(UI);
---
+++ js/interfaz.js
@@ -0,0 +1,6 @@
+'use strict';
+
+export const formSearch = document.querySelector('#formulario-buscar'),
+  divSearch = document.querySelector('#buscar'),
+  divMsj = document.querySelector('#mensajes'),
+  divResult = document.querySelector('#resultado');
