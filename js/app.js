77ab22d9a8fb7da42052da37f02243f744a3e550
laugeeme <registro.laura@gmail.com>
2020-06-19 13:16:36 +0200

    add msj if send empty values from form

--- js/app.js
+++ js/app.js
@@ -2,4 +2,21 @@
 
 import * as UI from './interfaz.js';
 
-console.log(UI);
+UI.formSearch.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  //get data from form
+  const artist = document.querySelector('#artista').value,
+    song = document.querySelector('#cancion').value;
+
+   if (artist === '' || song === ''){
+       UI.divMsj.innerHTML = 'Error... all fields are required';
+       UI.divMsj.classList.add('error');
+
+       setTimeout(()=>{
+
+        UI.divMsj.innerHTML='';
+        UI.divMsj.classList.remove('error');
+       }, 3000);
+   }
+});
